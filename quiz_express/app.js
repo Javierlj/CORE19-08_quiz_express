66cef2a390eb0279a74e772feb7f854c90ca5d77
Unknown <javierlopezjimeno@gmail.com>
2019-04-21 10:56:34 +0200

    Step 2: Create general layout

--- quiz_express/app.js
+++ quiz_express/app.js
@@ -3,10 +3,9 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var expressPartials = require('express-partials');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -18,9 +17,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(expressPartials());
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
