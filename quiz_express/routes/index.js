03ed3e281a01901f417e2bc964650c4892c01c12
Unknown <javierlopezjimeno@gmail.com>
2019-04-21 11:15:01 +0200

    Step 3: Credits

--- quiz_express/routes/index.js
+++ quiz_express/routes/index.js
@@ -5,5 +5,8 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+router.get('/credits', function(req, res, next) {
+  res.render('credits');
+});
 
 module.exports = router;
